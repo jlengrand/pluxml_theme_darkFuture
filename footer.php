11d1190b4e99b916bbc81583aa114df031a04390
Julien Lengrand-Lambert <julien@lengrandlambert.fr>
2011-11-17 11:02:36 +0100

    Switches to 1.2. XHTML validation and some other stuff. TODO : multinlingual posts

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 
 	<div id="footer">
 
-		<p>&copy;
+		<p>
 			<?php $plxShow->lang('POWERED_BY') ?> <a href="http://pluxml.org" title="<?php $plxShow->lang('PLUXML_DESCRIPTION') ?>">PluXml</a>
-			<span><a class="admin" rel="nofollow" href="<?php $plxShow->urlRewrite('core/admin/') ?>" title="<?php $plxShow->lang('ADMINISTRATION') ?>"><?php $plxShow->lang('ADMINISTRATION') ?></a>
+			<a class="admin" rel="nofollow" href="<?php $plxShow->urlRewrite('core/admin/') ?>" title="<?php $plxShow->lang('ADMINISTRATION') ?>"><?php $plxShow->lang('ADMINISTRATION') ?></a>
 		</p>
 	</div>
 </body>
--- sidebar.php
+++ sidebar.php
@@ -4,13 +4,12 @@
 		
 		<h3><?php $plxShow->lang('C_CONTACT') ?></h3>
 		<ul id="icons">
-            <li><a href="<?php echo GP ?>" target="_top"><img src="<?php echo GP_H ?>" style="border:0" alt="google plus profile" title="Find me on G+" height="40" width="40" /></a></li>
-            <li><a href="<?php echo VIADEO ?>" target="_top"><img src="<?php echo VIADEO_H ?>" style="border:0" alt="viadeo profile" title="Find me on Viadeo" height="40" width="40" /></a></li>
-            <li><a href="<?php echo LINKEDIN ?>" target="_top"><img src="<?php echo LINKEDIN_H ?>" style="border:0" alt="linkedin profile" title="Find me on Linkedin" height="40" width="40" /></a></li>
-            <li><a href="<?php echo TWITTER ?>" target="_top"><img src="<?php echo TWITTER_H ?>" style="border:0" alt="twitter profile" title="Follow me on Twitter" height="40" width="40" /></a></li>
-            <li><a href="<?php echo MAILTO ?>" target="_top"><img src="<?php echo MAILTO_H ?>" style="border:0" alt="mailto" title="Mail me!" height="40" width="40" /></a></li>
-            <!-- <li><a href="<?php $plxShow->urlRewrite('feed.php?rss') ?>" target="_top"><img src="<?php echo RSS_H ?>" style="border:0" alt="rss " title="Follow the news" height="40" width="40" /></a></li> -->
-            <li><a href="http://feeds.feedburner.com/LookUpToTheFuture" target="_top"><img src="<?php echo RSS_H ?>" style="border:0" alt="rss " title="Follow the news" height="40" width="40" /></a></li>
+            <li><a href="<?php echo GP ?>"><img src="<?php echo GP_H ?>" style="border:0" alt="google plus profile" title="Find me on G+" height="40" width="40" /></a></li>
+            <li><a href="<?php echo VIADEO ?>"><img src="<?php echo VIADEO_H ?>" style="border:0" alt="viadeo profile" title="Find me on Viadeo" height="40" width="40" /></a></li>
+            <li><a href="<?php echo LINKEDIN ?>"><img src="<?php echo LINKEDIN_H ?>" style="border:0" alt="linkedin profile" title="Find me on Linkedin" height="40" width="40" /></a></li>
+            <li><a href="<?php echo TWITTER ?>"><img src="<?php echo TWITTER_H ?>" style="border:0" alt="twitter profile" title="Follow me on Twitter" height="40" width="40" /></a></li>
+            <li><a href="<?php echo MAILTO ?>"><img src="<?php echo MAILTO_H ?>" style="border:0" alt="mailto" title="Mail me!" height="40" width="40" /></a></li>
+            <li><a href="http://feeds.feedburner.com/LookUpToTheFuture"><img src="<?php echo RSS_H ?>" style="border:0" alt="rss " title="Follow the news" height="40" width="40" /></a></li>
 		</ul>
 
         <h3><?php $plxShow->lang('C_NEWS') ?></h3>
@@ -26,11 +25,10 @@
 		    <li><a href="<?php echo DEBIAN ?>" title="<?php $plxShow->lang('C_DEB') ?>"><?php $plxShow->lang('C_DEB') ?></a></li>
 		    <li><a href="<?php echo STACK ?>" title="<?php $plxShow->lang('C_STACK') ?>"><?php $plxShow->lang('C_STACK') ?></a></li>
 		</ul>
-		
-        </br>
-        </br>
         
 		<a href="http://linuxcounter.net/" id="certificate">
-			<img src="<?php echo CERTIF_H ?>" title="linux certificate" >
+			<img alt="This image displayed my linux certificate as a certified linux user. " src="<?php echo CERTIF_H ?>" title="linux certificate" />
 		</a>
+                <a href="http://validator.w3.org/check?uri=referer" id="validation"><img
+                  src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
 </div>
